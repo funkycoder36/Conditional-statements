00f7449dbe106dbe9d245a6e49ed742b2c62472b
funkycoder36 <ifti80@yahoo.com>
2025-08-07 20:27:03 +0500

    Assignment

---
+++ Assignment.py
@@ -0,0 +1,7 @@
+text = str(input("Is it summer or winter: "))
+
+if text=="summer":
+  print("You should wear short sleeves and shorts as it is very hot in summer")
+
+else:
+  print("You should wear long clothes as the cold can harm u")
